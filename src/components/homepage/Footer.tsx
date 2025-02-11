268efaac1312dc445efd92f1b751c017c401bc73
unknown <mehmetcagriekici@gmail.com>
2025-02-11 17:21:41 +0300

    terms of usage

--- src/components/homepage/Footer.tsx
+++ src/components/homepage/Footer.tsx
@@ -9,8 +9,23 @@ import Image from "next/image";
 export default function Footer() {
   return (
     <footer className="relative w-full p-3 bg-red-800 flex flex-col justify-center items-center gap-5 dark:bg-emerald-900">
+      {/*Terms of Usage*/}
+      <section className="w-full flex justify-center items-center gap-3">
+        <p className="font-serifPrimary tracking-wide text-sm text-center text-sky-200 hover:cursor-pointer hover:text-yellow-200 hover:underline active:text-red-200">
+          Even though this is a small hobby project, you still need to create an
+          account to use the other features inside this app. With V0 relase,
+          these fetaures are writing reflections on daily readings, being able
+          to display the previous reflections, and edit your profile -adding an
+          avatar, or changing the location-. Once you login, your email, and the
+          data you shared in this application will be stored safely in a
+          supabase server, only I can reach. You can use this application
+          freely, you can copy the code in the public repositiory from my
+          github. Keep this in mind this is only a hobby project, and version 0
+          is pretty limited.
+        </p>
+      </section>
       {/*API*/}
-      <section className="w-full flex xjustify-center items-center gap-3">
+      <section className="w-full flex justify-center items-center gap-3">
         {/*LOGO*/}
         <Image
           src="https://files.evangelizo.org/images/website/logo/logo_2018_118.png"
